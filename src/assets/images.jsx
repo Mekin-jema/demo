fb942b5862e4c422ad6500c8c4ca853dec3ecb9d
Mekin Jemal <mekinjemal999@gmail.com>
2025-01-21 09:12:23 +0300

    fix some bugs

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -3,18 +3,17 @@ import download from "./Sidebar/download.svg";
 import gift from "./Sidebar/gift.svg";
 import grid from "./Sidebar/grid.png";
 import map from "./Sidebar/map.svg";
-import sidebar from "./Sidebar/Sidebar.svg";
+import Sidebar from "./Sidebar/sidebar.svg";
 import sliders from "./Sidebar/sliders.svg";
 import unlock from "./Sidebar/unlock.svg";
 
 const images = {
-  logo: new URL("/logo.png", import.meta.url).href,
   amba1: amba1,
   download: download,
   gift: gift,
   grid: grid,
   map: map,
-  sidebar: sidebar,
+  sidebar: Sidebar,
   sliders: sliders,
   unlock: unlock,
 };
